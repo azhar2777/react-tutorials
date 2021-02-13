53086c2903daa91fc19c49c6e448c16bb9fc0736
azharuddin <mailsahil07@gmail.com>
2021-02-13 20:27:36 +0530

    some alignment changed

--- screens/loginscreen.js
+++ screens/loginscreen.js
@@ -11,134 +11,134 @@ import {
     Alert,
     Button,
     TouchableOpacity,
-  } from 'react-native';
-  
-  import  Icon from 'react-native-vector-icons/FontAwesome5';
-  import Toast from 'react-native-simple-toast';
-  import loginStyle  from './css/mstyle';
-  import LinearGradient from 'react-native-linear-gradient';
+} from 'react-native';
 
-  export default class LoginScreen extends Component{
-      constructor(){
-          super()
-      }
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import Toast from 'react-native-simple-toast';
+import loginStyle from './css/mstyle';
+import LinearGradient from 'react-native-linear-gradient';
 
-    state = {
-        isButtonPressed:false,
-        email:"",
-        password:"",
+export default class LoginScreen extends Component {
+    constructor() {
+        super()
     }
 
-    handleSignupClick(){
-        
+    state = {
+        isButtonPressed: false,
+        email: "",
+        password: "",
+    }
+
+    handleSignupClick() {
+
         this.props.navigation.navigate('Signup')
 
     }
-    
-    handleEmail(email){
+
+    handleEmail(email) {
         this.setState({
-            email:email,
+            email: email,
         });
     }
-    handlePassword(password){
+    handlePassword(password) {
         this.setState({
-            password:password,
+            password: password,
         });
     }
 
-    handleButtonPressed(){
+    handleButtonPressed() {
 
         var email = this.state.email;
         var password = this.state.password;
-        if(email.trim() == ""){
+        if (email.trim() == "") {
             Toast.show('Please enter email');
             return;
         }
-        if(password.trim() == ''){
+        if (password.trim() == '') {
             Toast.show('Please enter password');
             return;
         }
     }
 
-    render(){
+    render() {
         return (
-<SafeAreaView style={{flex:1,}}>
-        <ImageBackground  source={require('../images/screen_bg.jpg')} style={loginStyle.mainContainer}>
-            <LinearGradient colors={['#499687','#ddd', ]}
-            // style={{backgroundColor:'#529599', flexDirection:'column', width:'100%', flex:1, alignSelf:'center', justifyContent:'center',}}
-            style={loginStyle.mainContainer2}
-            >
-                <View
-                style={loginStyle.logoStyle}
-                >
-                    <Text style={{alignSelf:'center', color:'white', fontSize:30}}>Put Logo Here</Text>
-                </View>
-                <View style={{ padding: 15, marginTop:30, alignContent:'stretch'}}>
-                    <TextInput 
-                    style={loginStyle.inputStyle}
-                    keyboardType="email-address"
-                    placeholder="Enter email"
-                    onChangeText={text => this.handleEmail(text)}
-                    />
-                    <View style={{height:20,}} />
-                    <TextInput 
-                    style={loginStyle.inputStyle}
-                    secureTextEntry={true}
-                    placeholder="Enter password"
-                    onChangeText={text => this.handlePassword(text)}
-                    />
-                <View style={{height:20,}} />
-                    <TouchableOpacity
-                    activeOpacity={0.5}
-
-                    style={loginStyle.buttonStyle}
-                    onPress= {()=>{
-                        this.handleButtonPressed()
-                    }}
+            <SafeAreaView style={{ flex: 1, }}>
+                <ImageBackground source={require('../images/screen_bg.jpg')} style={loginStyle.mainContainer}>
+                    <LinearGradient colors={['#499687', '#ddd',]}
+                        // style={{backgroundColor:'#529599', flexDirection:'column', width:'100%', flex:1, alignSelf:'center', justifyContent:'center',}}
+                        style={loginStyle.mainContainer2}
                     >
-                    <Text
-                        style={{
-                            color:'#FFFFFF',
-                        }}              
-                                        
-                                        >Submit</Text>
-                    </TouchableOpacity>
-                    <View style={{height:20,}} />
+                        <View
+                            style={loginStyle.logoStyle}
+                        >
+                            <Text style={{ alignSelf: 'center', color: 'white', fontSize: 30 }}>Put Logo Here</Text>
+                        </View>
+                        <View style={{ padding: 15, marginTop: 30, alignContent: 'stretch' }}>
+                            <TextInput
+                                style={loginStyle.inputStyle}
+                                keyboardType="email-address"
+                                placeholder="Enter email"
+                                onChangeText={text => this.handleEmail(text)}
+                            />
+                            <View style={{ height: 20, }} />
+                            <TextInput
+                                style={loginStyle.inputStyle}
+                                secureTextEntry={true}
+                                placeholder="Enter password"
+                                onChangeText={text => this.handlePassword(text)}
+                            />
+                            <View style={{ height: 20, }} />
+                            <TouchableOpacity
+                                activeOpacity={0.5}
 
-                    <View
-                    style={{flexDirection:'row', alignSelf:'center',}}
-                    >
-                    <Text
-                    
-                    style={{
-                        color:'#000',
-                       
-                    }}
-                    >Do'nt have an account ?</Text>
-                    <TouchableOpacity
-                        onPress={()=>{
-                            this.handleSignupClick();
-                        }}
-                    >
-                        <Text
-                        
-                        style={{
-                            color:'#FFF',
-                            fontWeight:'bold',
-                            alignSelf:'center',
-                            marginLeft:6,
-                        }}
-                        >Sign up here</Text>
-                    </TouchableOpacity>
-                    
-                    </View>
+                                style={loginStyle.buttonStyle}
+                                onPress={() => {
+                                    this.handleButtonPressed()
+                                }}
+                            >
+                                <Text
+                                    style={{
+                                        color: '#FFFFFF',
+                                    }}
 
-                    
-                </View>
-            </LinearGradient>
+                                >Submit</Text>
+                            </TouchableOpacity>
+                            <View style={{ height: 20, }} />
 
-        </ImageBackground>
-        </SafeAreaView>
+                            <View
+                                style={{ flexDirection: 'row', alignSelf: 'center', }}
+                            >
+                                <Text
+
+                                    style={{
+                                        color: '#000',
+
+                                    }}
+                                >Do'nt have an account ?</Text>
+                                <TouchableOpacity
+                                    onPress={() => {
+                                        this.handleSignupClick();
+                                    }}
+                                >
+                                    <Text
+
+                                        style={{
+                                            color: '#FFF',
+                                            fontWeight: 'bold',
+                                            alignSelf: 'center',
+                                            marginLeft: 6,
+                                        }}
+                                    >Sign up here</Text>
+                                </TouchableOpacity>
+
+                            </View>
+
+
+                        </View>
+                    </LinearGradient>
+
+                </ImageBackground>
+            </SafeAreaView>
         );
     }
-  }
+}
